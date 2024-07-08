96b2dd41e8b1c102ed871a5d910f343cc2d75ba3
MirFaizan01 <faizanmeer322@gmail.com>
2024-07-08 22:25:17 +0530

    completed theproject

--- app.js
+++ app.js
@@ -5,16 +5,27 @@ const http = require("http");
 const path = require("path");
 
 app.set(express.static(path.join(__dirname,"public")));
-app.set("veiw engine", "ejs");
+app.set("view engine", "ejs");
 
 
 const server = http.createServer(app);
 const io= socketio(server);
 
+io.on("connection",(socket)=>{
+    socket.on("send-location",(data)=>{
+        io.emit("recieve-location",{id:socket.id,...data});
+
+    })
+   socket.on("disconnect",()=>{
+    io.emit("user-disconnected",{id:socket.id})
+   })
+
+})
+
 
 
 app.get("/",(req,res)=>{
-    res.send("hey");
+    res.render("index");
 })
 
 app.listen(3000);
